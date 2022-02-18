39cd6753fecb7ea5e568231540a6f526c6fdfcd9
Horea Porutiu <hporutiu@slack-corp.com>
2022-02-17 21:14:18 -0800

    more refactor

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
-const { App, LogLevel } = require('@slack/bolt');
-const { registerListeners } = require('./listeners');
-const orgInstall = require('./database/auth/store-user-org-install')
-const workspace_auth = require('./database/auth/store-user-workspace-install')
-const html = require('./templates')
-const db = require('./database/db')
-let model = require('./database/db_model')
-const customRoutes = require('./utils/custom_routes')
-db.connect()
+const { App, LogLevel } = require("@slack/bolt");
+const { registerListeners } = require("./listeners");
+const orgInstall = require("./database/auth/store-user-org-install");
+const workspace_auth = require("./database/auth/store-user-workspace-install");
+const html = require("./templates");
+const db = require("./database/db");
+let model = require("./database/db_model");
+db.connect();
 
-// For development purposes only
-const tempDB = new Map();
-console.log(customRoutes)
 const app = new App({
   logLevel: LogLevel.DEBUG,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   clientId: process.env.SLACK_CLIENT_ID,
   clientSecret: process.env.SLACK_CLIENT_SECRET,
-  stateSecret: 'horea-is-a-human',
+  stateSecret: "horea-is-a-human",
   customRoutes: [
     {
-      path: "/slack/install/",
+      path: "/slack/install/workspace",
       method: ["GET"],
       handler: (req, res) => {
         res.writeHead(200);
-        res.end(html.normalInstall);
+        res.end(html.workspaceInstall);
       },
     },
     {
-        path: "/slack/install/userToken",
-        method: ["GET"],
-        handler: (req, res) => {
-          res.writeHead(200);
-          res.end(html.userTokenInstall);
-        },
+      path: "/slack/install/orgadmin",
+      method: ["GET"],
+      handler: (req, res) => {
+        res.writeHead(200);
+        res.end(html.orgAdminInstall);
       },
+    },
   ],
   installerOptions: {
     stateVerification: false,
   },
   installationStore: {
     storeInstallation: async (installation) => {
-
-      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-          console.log('you should only install after you have installed this app on the workspace level')
-          return await orgInstall(installation)
+      if (
+        installation.isEnterpriseInstall &&
+        installation.enterprise !== undefined
+      ) {
+        console.log(
+          "you should only install after you have installed this app on the workspace level",
+        );
+        return await orgInstall.saveUserOrgInstall(installation);
       }
       if (installation.team !== undefined) {
-        return await workspace_auth.saveUserWorkspaceInstall(installation)
+        return await workspace_auth.saveUserWorkspaceInstall(installation);
       }
-      throw new Error('Failed saving installation data to installationStore');
+      throw new Error("Failed saving installation data to installationStore");
     },
     fetchInstallation: async (installQuery) => {
-      console.log('inside fetchInstallation installQuery: ')
-      console.log(installQuery)
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        console.log('fetching enterprise install')
-        return tempDB.get(installQuery.enterpriseId);
+      console.log("inside fetchInstallation installQuery: ");
+      console.log(installQuery);
+      if (
+        installQuery.isEnterpriseInstall &&
+        installQuery.enterpriseId !== undefined
+      ) {
+        let user = await model.User.find({ _id: installQuery.teamId });
+        if (user[0] != undefined) {
+          return user[0];
+        }
+        return;
       }
       if (installQuery.teamId !== undefined) {
-
-        let user = await model.User.find({ _id: installQuery.teamId});
-        // console.log('user: ')
-        // console.log(user)
-        if (user[0]!= undefined){
-          return user[0]
+        let user = await model.User.find({ _id: installQuery.teamId });
+        if (user[0] != undefined) {
+          return user[0];
         }
       }
-      throw new Error('Failed fetching installation');
+      throw new Error("Failed fetching installation");
     },
   },
 });
@@ -78,8 +81,8 @@ registerListeners(app);
 (async () => {
   try {
     await app.start(process.env.PORT || 3000);
-    console.log('⚡️ Bolt app is running! ⚡️');
+    console.log("⚡️ Bolt app is running! ⚡️");
   } catch (error) {
-    console.error('Unable to start App', error);
+    console.error("Unable to start App", error);
   }
 })();
